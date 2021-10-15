233d2fbb3a7f439c73709e273effa34bb39b7b02
root <root@Zoe-Dell-Laptop.localdomain>
2021-10-15 21:12:10 +0800

    Added Post and Update Functionality, added to README

--- README.md
+++ README.md
@@ -19,6 +19,7 @@
   - [Python REST APIs with FastAPI, CRUD application](https://dev.to/xarala221/python-rest-apis-with-fastapi-crud-application-9kc)
   - [Building a CRUD APP with FastAPI and MySQL](https://blog.balasundar.com/building-a-crud-app-with-fastapi-and-mysql)
   - [Markdown cheat sheet](https://www.markdownguide.org/cheat-sheet/)
+  - [FastAPI Python WebAPI](https://realpython.com/fastapi-python-web-apis/)
 
 3. 我也需要下載FASTAPI，uvicorn, pip等等
 4. 我認爲用MYSQL資料庫很複雜，有一點麻煩，因爲需要設置其他的軟體（比如：SQLAcdemy,)，所以在第一迭代我不會用來可以注意學習FASTAPI. 我會用Python的串列存儲消息
@@ -26,7 +27,10 @@
   - 開始編寫程式的時候，我也創造新的Github repository，所以我可以開始追蹤我的進程
   - 首先用FASTAPI Introduction網站的例子來試試怎麽用FASTAPI 和 unicorn
   - 其次開始寫自己的程式，選擇在那個迭代不會用MySQL資料庫，會用Python串列
-  - 然後選擇我的資料庫内容的例子是人們的消息, 寫personDB的變數和Person的class
-  - 然後寫第一個READ的函數  
+  - 然後選擇我的資料庫内容的例子是人們的消息, 寫`personDB`的變數和`Person`的class
+  - 然後寫第一個READ的函數，選擇我應該有兩個READ函數，所以人們可以找一個人或者存取每個人的消息
+  - 然後我寫一個Create和Update函數，在這兩函數您可以加入人的消息，但是我也想加入Person的object的能力所以我寫了第二個Create和Update函數
+  - 因爲我在第二個Create和Update的函數調用第一個Create和Update的函數，我不可以用async的函數所以我把async函數改變普通函數
+  
 
 6. 編寫程式的時候，同時寫comments,所以我和其他的人可以知道我做什麽
--- main.py
+++ main.py
@@ -15,9 +15,10 @@ class Person (BaseModel):
   Nationality: str
   created_time: datetime = datetime.now()
 
+
 # 第一個READ函數 （function)，返回personDB的内容
 @app.get("/Persons")
-async def get_persons():
+def get_persons():
     return personDB
 
 # 幫助函數 - 在personDB從一個id,找到一個Person
@@ -30,18 +31,56 @@ def findPerson(pid):
 
 # 第二個READ函數，返回具體的Person
 @app.get("/Person/{person_id}")
-async def get_person(person_id: int):
+def get_person(person_id: int):
   index = findPerson(person_id)
   if index > -1:
     return personDB[index]
   return {"message": "That person is not in the database 這個人不在資料庫"}
 
-# 第一Create函數，把新的Person消息加入在personDB裏
+# 第一Create函數，把新的Person的oject加入在personDB裏
+@app.post("/Add_Person_Item")
+def add_person_item(person: Person):
+  index = findPerson(person.id)
+  if index == -1: # 如果人已經在資料庫，會改變人的消息
+    update_person_item(person)
+  personDB.append(person)
+  return {"message": "Person added 人加入了"}
+
+# 第二Create函數，如果沒有Person的object，還可以加入人
 @app.post("/Add_Person")
-async def add_person(id: int, name: str, age: int, nat: str, time: Optional[datetime] = datetime.now()):
-  
+def add_person(id: int, name: str, age: int, nat: str, time: Optional[datetime] = datetime.now()):
+  data = {
+    "id": id,
+    "Name": name,
+    "Age": age,
+    "Nationality": nat,
+    "created_time": time
+  }
+  tempPerson = Person(**data) # 把人的消息改成Person的oject
+  # print(tempPerson)
+  return (add_person_item(tempPerson)) # 用已經寫的函數來加入人的消息
 
+# 第一Update函數，會改變人的消息
+@app.put("/Update_Person_Item")
+def update_person_item(person: Person):
+  index = findPerson(person.id)
+  if index != -1:
+    personDB[index] = person
+    return {"message": "Person has be updated 人的消息改變了"}
+  return {"message": "Person does not exist in the database 人不在資料庫裏"}
 
-# @app.update
+# 第二Update函數，如果沒有Person的object，還可以改變
+@app.put("/Update_Person")
+def update_person(id: int, name: str, age: int, nat: str, time: Optional[datetime] = datetime.now()):
+  data = {
+    "id": id,
+    "Name": name,
+    "Age": age,
+    "Nationality": nat,
+    "created_time": time
+  }
+  tempPerson = Person(**data)
+  return (update_person_item(tempPerson))
+
 #
 # @app.delete
