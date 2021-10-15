ceeecaac9c0b5db34d44eec625a2e557dc9f3203
root <root@Zoe-Dell-Laptop.localdomain>
2021-10-15 13:36:36 +0800

    Updated README, Started Coding

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI ## Import Fast API
+from pydantic import BaseModel ## Import data validation
+from datetime import datetime
 app = FastAPI() ## Create FastAPI instance
 
 ## 在這個作業，我會用Python的list (串列)來存儲消息
-
 db = []
 
+## Python class，代表資料庫的欄
+class Person (BaseModel):
+  id: int # 我會說這是Primary Key
+  Name: str
+  Age: int
+  Nationality: str
 
 @app.get("/")
 def first_example():
